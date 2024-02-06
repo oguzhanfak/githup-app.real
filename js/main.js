eb421ecce2a49d260c95c8dc0bfbb83618e50697
Oguzhan <126396389+oguzhanfak@users.noreply.github.com>
2024-02-07 00:14:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const githubName = document.querySelector("#githubname")
+const form = document.querySelector("#searchForm")
+const clearButton = document.querySelector("#clearButton")
+const clearAllButton = document.querySelector("#clearAllButton")
+
+const github = new Github();
+const ui = new UI();
+
+runEventListeners();
+
+function runEventListeners(){
+    form.addEventListener("submit",search)
+    clearButton.addEventListener("click",  clearInput)
+    document.addEventListener("DOMContentLoaded",runPageLoaded)
+    clearAllButton.addEventListener("click",clearSearchedUser)
+}
+function clearSearchedUser(){
+    Storagex.clearAllSearchedUsersFromStorage()
+    ui.clearSearchedUsers()
+}
+function runPageLoaded(){
+    ui.fillSearchedUserToUIFromStorage()
+}
+function clearInput(){
+    ui.clearInput();
+}
+function search(e){
+const username = githubName.value.trim()
+if(username==null || username==""){
+    alert("lütfen bir kullanıcı adı giriniz")
+}else{
+github.getGithubData(username)
+.then(response => {
+    ui.addSearhedUserToUI(username)
+    Storagex.addSearchedUserToStorage(username)
+    ui.addUserProfileToUI(response.user);
+    document.querySelector("#showRepo").addEventListener("click",()=> ui.showRepos(response.repo));
+})
+.catch(error => console.log(error))
+}
+    e.preventDefault();
+}
